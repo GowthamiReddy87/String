89fe90842471c2f5aa62fad067ca61cb811d18a4
GOWTHAMI REDDY SIRIGI REDDY <gowthamireddy1772@gmail.com>
2026-07-14 07:46:36 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,15 @@
+public class ReverseString
+{
+public static void reverse(String s)
+{
+for(int i=s.length()-1;i>=0;i--)
+{
+System.out.print(s.charAt(i));
+}
+}
+public static void main(String[] args)
+{
+String s="hello";
+reverse(s);
+}
+}
