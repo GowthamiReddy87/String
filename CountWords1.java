81b11fa16aea8b49ba7570a35bbf983ec442e355
GOWTHAMI REDDY SIRIGI REDDY <gowthamireddy1772@gmail.com>
2026-07-21 07:37:52 +0530

    Add files via upload

---
+++ CountWords1.java
@@ -0,0 +1,26 @@
+public class CountWords1
+{
+public static void count(String s)
+{
+int words=0;
+for(int i=0;i<s.length();i++)
+{
+char ch=s.charAt(i);
+
+if(Character.isLetter(ch))
+{
+
+if(Character.isWhitespace(ch))
+{
+words++;
+}
+}
+}
+System.out.println("No of words : "+words);
+}
+public static void main(String[] args)
+{
+String s="i am good girl";
+count(s);
+}
+}
